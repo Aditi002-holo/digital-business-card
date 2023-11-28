1218b7fbf5b74d6b21b2a204fd0fecc9bdd448a6
Aditi Deshpande <aditideshpande2908@gmail.com>
2023-11-28 17:37:16 +0530

    Fix: images

--- src/Images/Images.jsx
+++ src/Images/Images.jsx
@@ -3,7 +3,7 @@ const IMAGES = {
     TwitterIcon: new URL('./Twitter-Icon.svg', import.meta.url).href,
     MetaIcon: new URL('./Meta-Icon.svg', import.meta.url).href,
     InstagramIcon: new URL('./Instagram-Icon.svg', import.meta.url).href,
-    GithubIcon: new URL('./Github-Icon.svg', import.meta.url).href,
+    GithubIcon: new URL('./GitHub-Icon.svg', import.meta.url).href,
     EmailIcon: new URL('./Email-Icon.svg', import.meta.url).href,
     LinkedInIcon: new URL('./Linkedin-Icon.svg', import.meta.url).href
 }
